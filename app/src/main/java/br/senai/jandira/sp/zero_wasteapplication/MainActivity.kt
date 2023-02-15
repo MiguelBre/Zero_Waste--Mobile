34066d1ac3d4c25eb5e6366311b8b78cedf60b23
MIGUEL ANTONIO MEDEIROS BRESSAN <22102406@dsn127m1241955.cfp127aluno.local>
2023-02-15 10:30:12 -0300

    15/02/2023

---
+++ app/src/main/java/br/senai/jandira/sp/zero_wasteapplication/MainActivity.kt
@@ -0,0 +1,126 @@
+package br.senai.jandira.sp.zero_wasteapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.jandira.sp.zero_wasteapplication.ui.theme.Zero_WasteApplicationTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Zero_WasteApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(8, 113, 19)
+                ) {
+                    Column(
+
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.app_logo),
+                                contentDescription = "App Logo",
+                                modifier = Modifier
+                                    .size(96.dp)
+                                    .padding(end = 13.dp)
+                            )
+                            Text(
+                                text = stringResource(id = R.string.name_application),
+                                color = Color.White,
+                                fontSize = 36.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.comunity_question),
+                                modifier = Modifier.padding(bottom = 8.dp),
+                                color = Color.White,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Medium
+                            )
+                            Text(
+                                text = stringResource(id = R.string.faca_login),
+                                modifier = Modifier.padding(start = 30.dp, end = 30.dp),
+                                color = Color.White,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Normal,
+                                textAlign = TextAlign.Center
+                            )
+                            Button(
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 60.dp, end = 60.dp),
+                                shape = RoundedCornerShape(40.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = Color(
+                                        128,
+                                        204,
+                                        40
+                                    )
+                                )
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.login),
+                                    modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
+                                    color = Color.White,
+                                    fontSize = 20.sp
+                                )
+                            }
+                            Image(
+                                painter = painterResource(id = R.drawable.catadores_image),
+                                contentDescription = "",
+                                modifier = Modifier.size(width = 300.dp, height = 300.dp)
+                            )
+                            Text(
+                                text = stringResource(id = R.string.not_registered),
+                                color = Color.White,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Zero_WasteApplicationTheme {
+        Greeting("Android")
+    }
+}
